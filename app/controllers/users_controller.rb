055cef4f11d6e07a685518974e05b51b290caf6e
Joshua Burger <joshberger6969@outlook.com>
2021-10-03 22:53:08 -0400

    Create new index, create, show, and update, Index renders all users in JSON

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def update
+
+    end
+
+
+end
