962d4e47d40af7d779c563c0df695777e5af7770
Joshua Burger <joshberger6969@outlook.com>
2021-10-06 18:46:55 -0400

    Find user by name and password as well as avatar attached to activestorage blob within user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,15 @@ class UsersController < ApplicationController
     end
 
     def show
+        user = User.find_by(name: params[:name])
+        avatar = rails_blob_path(user.avatar)
 
+
+        if user.password == params[:password]
+            render json: {user: user, avatar: avatar}
+        else
+            render json: { message: 'This user is not authenticated'}
+        end
     end
 
     def update
