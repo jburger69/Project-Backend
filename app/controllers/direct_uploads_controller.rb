f92aa83fd6e5282a0d4f4c80f20aaafae103d454
Joshua Burger <joshberger6969@outlook.com>
2021-10-08 20:47:15 -0400

    Create private method to upload blob as json()

--- app/controllers/direct_uploads_controller.rb
+++ app/controllers/direct_uploads_controller.rb
@@ -3,7 +3,15 @@ class DirectUploadsController < ActiveStorage::DirectUploadsController
 
     def create
         blob = ActiveStorage::Blob.create_before_direct_upload!(blob_args)
-        byebug
+    end
+
+    private
+
+    def direct_upload_json(blob)
+        blob.as_json(root: false, methods: :signed_id).merge(service_url: url_for(blob)).merge(direct_upload: {
+            url: blob.service_url_for_direct_upload,
+            headers: blob.service_headers_for_direct_upload
+        })
     end
 
 end
